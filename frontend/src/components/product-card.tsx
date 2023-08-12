8c08ce006e5ac91d83deacbfda8b77b176ffb926
Namkhang Tsamchoe <ntnamkhang@gmail.com>
2023-08-11 23:36:49 -0400

    chore: add additional arrays in backend for different book categories

---
+++ frontend/src/components/product-card.tsx
@@ -0,0 +1,56 @@
+"use client";
+import Link from "next/link";
+
+export const ProductCard = ({ product }) => {
+  const { id, name, overview, poster, price, rating, best_seller } = product;
+
+  return (
+    <div className="m-5 max-w-xs bg-white rounded-lg border border-black shadow-md dark:bg-gray-800 dark:border-black">
+      <Link href={`/products/${id}`} className="relative">
+        {best_seller && (
+          <span className="absolute top-4 left-2 px-2 bg-orange-500 bg-opacity-90 text-white rounded">
+            Best Seller
+          </span>
+        )}
+        <img className="rounded-t-lg w-full h-64" src={poster} alt={name} />
+      </Link>
+      <div className="p-5">
+        <Link href={`/products/${id}`}>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {name}
+          </h5>
+        </Link>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {overview}
+        </p>
+
+        {/* <div className="flex items-center my-2">
+          <Rating rating={rating} />
+        </div> */}
+
+        <p className="flex justify-between items-center">
+          <span className="text-2xl dark:text-gray-200">
+            <span>$</span>
+            <span>{price}.00</span>
+          </span>
+          {/* {!inCart && (
+            <button
+              onClick={() => addToCart(product)}
+              className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800"
+            >
+              Add To Cart <i className="ml-1 bi bi-plus-lg"></i>
+            </button>
+          )}
+          {inCart && (
+            <button
+              onClick={() => removeFromCart(product)}
+              className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800"
+            >
+              Remove Item <i className="ml-1 bi bi-trash3"></i>
+            </button>
+          )} */}
+        </p>
+      </div>
+    </div>
+  );
+};

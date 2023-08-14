a8a60ff712444ee09721b70d601a185237f98be3
Namkhang Tsamchoe <ntnamkhang@gmail.com>
2023-08-14 10:07:05 -0400

    chore: improve ui of the home page

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <div className="bgimg">
           <div className="bg-white/60">
             <Header />
-            {children} <Footer />{" "}
+            {children} <Footer />
           </div>{" "}
         </div>
       </body>

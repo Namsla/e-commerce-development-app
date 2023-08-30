a614c02a0e581ccc2fecb866d38eeef8be27dd23
Namkhang Tsamchoe <ntnamkhang@gmail.com>
2023-08-30 17:25:58 -0400

    fix: improve ui of the wishlist page and imrove the ui of the header too

--- frontend/src/app/wishlist/page.tsx
+++ frontend/src/app/wishlist/page.tsx
@@ -1,34 +1,22 @@
 "use client";
-import Link from "next/link";
-import { useWishList } from "../../context/WishListContext";
+import { useWishList } from "@/context/WishListContext";
+import React from "react";
+import { WishList } from "./wish-list";
 
-export const WishList = ({ product }) => {
-  const { removeFromWishList } = useWishList();
+export const WishListPage = () => {
+  const { wishList } = useWishList();
 
   return (
-    <div className="flex flex-wrap justify-between border-b dark:border-slate-700 max-w-5xl m-auto p-2 mb-5 h-screen">
-      <div className="flex">
-        <a href="">
-          <img
-            className="w-32 rounded"
-            src={product?.poster}
-            alt={product?.name}
-          />
-        </a>
-        <div className="">
-          <Link href={`products/${product?.id}`}>
-            <p className="text-lg ml-2 dark:text-slate-200">{product?.name}</p>
-          </Link>
-          <button
-            onClick={() => removeFromWishList(product)}
-            className="text-base ml-2 text-red-400"
-          >
-            Remove From Wish List
-          </button>
+    <>
+      <div className="h-screen">
+        <div className="dark:bg-gray-800 flex flex-col items-center mt-10">
+          <main>
+            <WishList />
+          </main>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
-export default WishList;
+export default WishListPage;

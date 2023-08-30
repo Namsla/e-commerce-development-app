94b69e3176cf9129f0c83acba91582d353956e5a
Namkhang Tsamchoe <ntnamkhang@gmail.com>
2023-08-29 20:22:02 -0400

    fix: improve shopping cart ui

--- frontend/src/app/product-checkout-page/card-checkout.tsx
+++ frontend/src/app/product-checkout-page/card-checkout.tsx
@@ -1,11 +1,13 @@
 import { useCart } from "../../context/CardContext";
+import Link from "next/link";
+// import { ToastContainer, toast } from "react-toastify";
 
 export const Checkout = ({ setCheckout }) => {
   const { total } = useCart();
 
   return (
     <section>
-      <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50"></div>
+      <div className="fixed top-0 left-0 w-full h-full bg-white-100"></div>
       <div
         id="authentication-modal"
         tabIndex="-1"
@@ -70,7 +72,7 @@ export const Checkout = ({ setCheckout }) => {
                     name="email"
                     id="email"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:value-gray-400 dark:text-white"
-                    value="namsan@gmail.com"
+                    value="namsla@gmail.com"
                     disabled
                     required=""
                   />
@@ -135,15 +137,17 @@ export const Checkout = ({ setCheckout }) => {
                     required=""
                   />
                 </div>
-                <p className="mb-4 text-2xl font-semibold text-lime-500 text-center">
-                  ${total}
+                <p className="mb-4 text-2xl semi-bold text-lime-500 text-center">
+                  ${total}.00
                 </p>
-                <button
-                  type="submit"
-                  className="w-full text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700"
-                >
-                  <i className="mr-2 bi bi-lock-fill"></i>PAY NOW
-                </button>
+                <Link href={`products`}>
+                  <button
+                    type="submit"
+                    className="w-full text-white bg-blue-700 hover:bg-blue-800 font-bold rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700"
+                  >
+                    <i className="mr-2 bi bi-lock-fill"></i>PAY NOW
+                  </button>
+                </Link>
               </form>
             </div>
           </div>
